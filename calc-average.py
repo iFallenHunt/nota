3848366c886413166386bc5a798279ae1b7fe32d
iFallenHunt <lukas_santos.silva@hotmail.com>
2022-07-15 17:02:19 -0300

    codigo corrigido

--- calc-average.py
+++ calc-average.py
@@ -1,57 +1,16 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "calculo de media",
-      "provenance": [],
-      "authorship_tag": "ABX9TyPKppSIJPU1Lr5oEUoMaX9a",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/iFallenHunt/nota/blob/main/calculo_de_media.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "SYv8NXrjuryQ"
-      },
-      "outputs": [],
-      "source": [
-        "m1 = float(input('Digite a nota M1: '))\n",
-        "m2 = float(input('Digite a nota M2: '))\n",
-        "m3 = float(input('Digite a nota M3: '))\n",
-        "\n",
-        "media = (m1 + m2 + m3) / 3 \n",
-        "\n",
-        "if media >= 0.0 and media <= 4.0:\n",
-        "  print('Aluno reprovado')\n",
-        "elif media >= 4.1 and media <= 6.0:\n",
-        "  exame = float(input('Digite a nota do exame'))\n",
-        "  if exame >= 6.0:\n",
-        "    print('Aprovado no exame')\n",
-        "  else:\n",
-        "    print('Reprovado no exame') \n",
-        "else:\n",
-        "  print('Aluno aprovado')    "
-      ]
-    }
-  ]
-}
+m1 = float(input('Digite a nota M1: '))
+m2 = float(input('Digite a nota M2: '))
+m3 = float(input('Digite a nota M3: '))
+
+media = (m1 + m2 + m3) / 3
+
+if media >= 0.0 and media <= 4.0:
+  print('Aluno reprovado')
+elif media >= 4.1 and media <= 6.0:
+  exame = float(input('Digite a nota do exame'))
+  if exame >= 6.0:
+    print('Aprovado no exame')
+  else:
+    print('Reprovado no exame')
+else:
+  print('Aluno aprovado')
